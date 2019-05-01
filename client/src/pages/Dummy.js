e6882f6cc2ff792e35198cb073ef8372aa47bdc6
irfanp90 <irfanp90@gmail.com>
2019-05-01 10:33:38 -0500

    update

--- client/src/pages/Dummy.js
+++ client/src/pages/Dummy.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Search () {
+function Dummy () {
     return(
 
         <div>hello</div>
@@ -8,4 +8,4 @@ function Search () {
     
 }
 
-export default Search;
+export default Dummy;
