d5fe4ef8f0594ef5f5ff7963738bdb41bed6089d
irfanp90 <irfanp90@gmail.com>
2019-05-01 10:30:55 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Search from "./pages/Search"
-import Search from "./pages/Search"
+import Search1 from "./pages/Search"
 import Saved from "./pages/Saved"
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         
 
         <Switch>
-          <Route exact path="/" component={Search} /> 
+          <Route exact path="/" component={Search1} /> 
           <Route exact path="/saved" component={Saved} />
           {/* <Route exact path="/books/:id" component={Detail} />
           <Route component={NoMatch} /> */}
